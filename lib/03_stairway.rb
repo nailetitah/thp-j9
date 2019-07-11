46bc29faec725c7fd4d2afec4bd3dae233ea8c9c
ntitah <naile.titah@gmail.com>
2019-07-11 15:29:14 +0200

    Modifications sur stairway & ajout de readme

--- lib/03_stairway.rb
+++ lib/03_stairway.rb
@@ -7,46 +7,47 @@ def jeu
 win = 10
 run = 0
 counter = 0
-
-while run != win
-user_dice = rand(1..6)
-if user_dice == 5 || user_dice == 6
-	run += 1
-	counter += 1
-	puts "Vous montez d'une marche !"
-	puts "Vous êtes à la marche n°#{run}"
-elsif user_dice == 1
-	counter += 1
-	if run == 0
-		run = 0
-		puts "Vous restez en bas des marches."
-	else
-	run -= 1
-	puts "Vous descendez d'une marche."
+	while run != win
+	user_dice = rand(1..6)
+		if user_dice == 5 || user_dice == 6
+		run += 1
+		counter += 1
+		puts "Vous montez d'une marche !"
+		puts "Vous êtes à la marche n°#{run}"
+		elsif user_dice == 1
+		counter += 1
+			if run == 0
+			run = 0
+			puts "Vous restez en bas des marches."
+			else
+			run -= 1
+			puts "Vous descendez d'une marche."
+			end
+		elsif user_dice == 2 || user_dice == 3 || user_dice == 4
+		puts "Rien ne se passe."
+		counter += 1
+			if run >= 1
+			puts "Vous êtes à la marche n°#{run}"
+			else 
+			puts "Vous êtes toujours en bas des marches."
+			end
+		end
 	end
-elsif user_dice == 2 || user_dice == 3 || user_dice == 4
-	puts "Rien ne se passe."
-	counter += 1
-	if run >= 1
-	puts "Vous êtes à la marche n°#{run}"
-	else 
-	puts "Vous êtes toujours en bas des marches."
-end
-end
-end
-puts "Bravo, vous avez gagné !"
-return counter
+	puts "Bravo, vous avez gagné !"
+	return counter
 end
 
-def hundredGames(rec)
-	games = rec.times { |i| jeu }
+def average_finish_time
+parties = 0
+total_parties = 100
+total_counters = 0
+while parties != total_parties
+	jeu
+	total_counters += jeu
+	parties += 1
+end
+avg = total_counters / total_parties
+puts "Total de parties en moyenne = #{avg}"
 end
 
-def counterPlayed(games)
-	puts "Nombre de parties jouées : #{games}"
-end
-
-def perform
-	counterPlayed(jeu)
-end
-
+average_finish_time
